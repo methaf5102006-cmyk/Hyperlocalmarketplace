cdedde14b58524ccab701dcd6618f1c40835fb2f
methaf5102006-cmyk <methaf5102006@gmail.com>
2026-08-22 09:48:01 +0500

    fix base path for vercel deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Hyperlocalmarketplace/'
-})
+  // eslint-disable-next-line no-undef
+  base: process.env.VERCEL ? '/' : '/Hyperlocalmarketplace/',
+})  
